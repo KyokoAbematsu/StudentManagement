8f50b1fffc842e9c15e183504947f828cca235c7
Kyoko Abematsu <kyonpi0518@icloud.com>
2026-02-07 14:05:26 +0900

    Add application source code

---
+++ src/main/java/raisetech/StudentsManagement/StudentsManagementApplication.java
@@ -0,0 +1,27 @@
+package raisetech.StudentsManagement;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentsManagement.data.Student;
+import raisetech.StudentsManagement.data.StudentCourse;
+
+
+@SpringBootApplication
+@RestController
+public class StudentsManagementApplication<name> {
+
+  @Autowired
+  private StudentRepository2 repository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentsManagementApplication.class, args);
+
+  }
+
+  }
+
