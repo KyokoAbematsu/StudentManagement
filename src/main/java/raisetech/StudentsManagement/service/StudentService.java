4ed0dd0f0c5a30186a3384306b28c2e43aed65f4
Kyoko Abematsu <kyonpi0518@icloud.com>
2026-03-05 11:58:09 +0900

    第15回課題 受講生登録機能の実装

--- src/main/java/raisetech/StudentsManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentsManagement/service/StudentService.java
@@ -3,28 +3,51 @@ package raisetech.StudentsManagement.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import raisetech.StudentsManagement.StudentCourseRepository;
 import raisetech.StudentsManagement.StudentRepository2;
 import raisetech.StudentsManagement.data.Student;
 import raisetech.StudentsManagement.data.StudentCourse;
+import org.springframework.transaction.annotation.Transactional;
+import raisetech.StudentsManagement.domain.StudentDetail;
 
 @Service
 public class StudentService {
 
-  private StudentRepository2 repository;
+  private StudentRepository2 studentRepository;
+  private StudentCourseRepository courseRepository;
 
   @Autowired
-  public StudentService(StudentRepository2 repository) {
-    this.repository = repository;
+  public StudentService(StudentRepository2 studentRepository, StudentCourseRepository courseRepository) {
+    this.studentRepository = studentRepository;
+    this.courseRepository = courseRepository;
   }
-
 
   public List<Student> searchStudentList() {
-    return repository.search();
+    return studentRepository.search();
   }
-
 
   public List<StudentCourse> searchStudentCourseList() {
-    return repository.searchStudentCourse();
+    return courseRepository.findAll();
   }
-}
+
+  public void registerStudent(Student student) {
+    studentRepository.insertStudent(student);
+  }
+
+  @Transactional
+  public void registerStudentwithCourses(StudentDetail detail) {
+    Student student = detail.getStudent();
+    studentRepository.insertStudent(student);
+    if (detail.getStudentCourses() ==null) return;
+
+    for (StudentCourse c : detail.getStudentCourses()) {
+      if (c == null || c.getCourseName() == null || c.getCourseName().isBlank())
+        continue;
+
+      c.setStudentId(student.getId());
+      courseRepository.insertStudentCourse(c);
+    }
+    }
+
+  }
 
