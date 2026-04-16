d667ffd3f2bf96053fc126f45fe3dabbc2659bd3
Kyoko Abematsu <kyonpi0518@icloud.com>
2026-04-16 15:14:46 +0900

    第21回課題 受講生登録およびコース登録機能の実装と修正

--- src/main/java/raisetech/StudentsManagement/StudentRepository2.java
+++ src/main/java/raisetech/StudentsManagement/StudentRepository2.java
@@ -9,17 +9,48 @@ import org.apache.ibatis.annotations.Update;
 import raisetech.StudentsManagement.data.Student;
 import raisetech.StudentsManagement.data.StudentCourse;
 
+/**
+ * 受講生テーブルと受講生コース情報テーブルと紐づくRepositoryです。
+ */
+
 @Mapper
 public interface StudentRepository2 {
+
+  /**
+   * 受講生の全件検索を行います。
+   *
+   * @return　受講生一覧（全件）
+   */
 
   @Select("SELECT * FROM students WHERE is_deleted = false")
   List<Student> search();
 
+  /**
+   * 受講生の検索を行います。
+   *
+   * @param id　受講生ID
+   * @return　受講生
+   */
+
   @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(String id);
 
+  /**
+   *
+   *受講生のコース情報の全件検索を行います。
+   *
+   * @return　受講生のコース情報（全件）
+   */
+
   @Select("SELECT * FROM students_courses")
   List<StudentCourse> searchStudentCourseList();
+
+  /**
+   * 受講生IDに紐づく受講生コース情報を検索します。
+   *
+   * @param studentId　受講生ID
+   * @return　受講生IDに紐づく受講生コース情報
+   */
 
   @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
   List<StudentCourse> searchStudentCourse(String studentId);
@@ -31,7 +62,7 @@ public interface StudentRepository2 {
   void registerStudent(Student student);
 
   @Insert(
-      "INSERT INTO students_courses(student_id, course_name, course_start_at, course_end_at) "
+      "INSERT INTO students_courses(student_id, course_name, start_date, end_date) "
           + "VALUES(#{studentId}, #{courseName}, #{courseStartAt}, #{courseEndAt})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudentCourse(StudentCourse studentCourse);
--- src/main/java/raisetech/StudentsManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentsManagement/service/StudentService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import raisetech.StudentsManagement.StudentCourseRepository;
 import raisetech.StudentsManagement.StudentRepository2;
 import raisetech.StudentsManagement.data.Student;
 import raisetech.StudentsManagement.data.StudentCourse;
@@ -15,15 +14,18 @@ import raisetech.StudentsManagement.domain.StudentDetail;
 public class StudentService {
 
   private StudentRepository2 studentRepository;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentService(StudentRepository2 studentRepository,
-      StudentCourseRepository courseRepository) {
+  public StudentService(StudentRepository2 studentRepository, StudentConverter converter) {
     this.studentRepository = studentRepository;
+    this.converter = converter;
   }
 
-  public List<Student> searchStudentList() {
-    return studentRepository.search();
+  public List<StudentDetail> searchStudentList() {
+    List<Student> studentList = studentRepository.search();
+    List<StudentCourse> studentCourseList = studentRepository.searchStudentCourseList();
+    return converter.convertStudentDetails(studentList, studentCourseList);
   }
 
   public List<StudentCourse> searchStudentCourseList() {
@@ -32,25 +34,27 @@ public class StudentService {
 
   public StudentDetail searchStudent(String id) {
     Student student = studentRepository.searchStudent(id);
-    List<StudentCourse> studentCourses = studentRepository.searchStudentCourse(student.getId());
+    List<StudentCourse> studentCourses =
+        studentRepository.searchStudentCourse(String.valueOf(student.getId()));
+
     StudentDetail studentDetail = new StudentDetail();
     studentDetail.setStudent(student);
     studentDetail.setStudentCourses(studentCourses);
-    return studentDetail;
-  }
 
-  public void registerStudent(Student student) {
-    studentRepository.registerStudent(student);
+    return studentDetail;
   }
 
   @Transactional
   public void registerStudent(StudentDetail studentDetail) {
     studentRepository.registerStudent(studentDetail.getStudent());
-    for (StudentCourse studentCourse : studentDetail.getStudentCourses()) {
-      studentCourse.setStudentId(studentDetail.getStudent().getId());
-      studentCourse.setCourseStartAt(LocalDateTime.now());
-      studentCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
-      studentRepository.registerStudentCourse(studentCourse);
+
+    if (studentDetail.getStudentCourses() != null) {
+      for (StudentCourse studentCourse : studentDetail.getStudentCourses()) {
+        studentCourse.setStudentId(String.valueOf(studentDetail.getStudent().getId()));
+        studentCourse.setCourseStartAt(LocalDateTime.now());
+        studentCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
+        studentRepository.registerStudentCourse(studentCourse);
+      }
     }
   }
 
@@ -60,7 +64,7 @@ public class StudentService {
 
     if (studentDetail.getStudentCourses() != null) {
       for (StudentCourse studentCourse : studentDetail.getStudentCourses()) {
-        studentCourse.setStudentId(studentDetail.getStudent().getId());
+        studentCourse.setStudentId(String.valueOf(studentDetail.getStudent().getId()));
         studentRepository.updateStudentCourses(studentCourse);
       }
     }
